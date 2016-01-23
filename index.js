facf019e7284339f66c2d00fd8ce4e32e810c8b4
Michael Malura <maluramichael@googlemail.com>
2016-01-23 10:32:11 +0100

    Add image download function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var http = require('http'),
 	lodash = require('lodash'),
+	fs = require('fs'),
+	request = require('request'),
 	mkdirp = require('mkdirp');
 
 const filterExtensions = ['png', 'jpg', 'gif'];
@@ -10,32 +12,69 @@ const pathContainsExtension = path=> {
 		}).value().length > 0;
 };
 
-http.createServer(function (request, response) {
+const dataDirectory = 'data/';
+
+var download = function (uri, filename, callback) {
+	request.head(uri, function (err, res, body) {
+		console.log('content-type:', res.headers['content-type']);
+		console.log('content-length:', res.headers['content-length']);
+
+		request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+	});
+};
+
+var downloadQueue = [];
+
+http.createServer(function (req, res) {
 	// var proxy = http.createClient(80, request.headers['host'])
 	// var proxy_request = proxy.request(request.method, request.url, request.headers);
 	var proxy_request = http.request({
 		port: 80,
-		host: request.headers['host'],
-		method: request.method,
-		path: request.url,
-		headers: request.headers
+		host: req.headers['host'],
+		method: req.method,
+		path: req.url,
+		headers: req.headers
 	});
 
-	console.log(`[${request.method}] ${request.url} ${pathContainsExtension(request.url)}`);
+	if (pathContainsExtension(req.url)) {
+		var directories = req.url.split('/');
+		directories = lodash(directories).filter(e=> {
+			switch (e) {
+				case '':
+				case 'http:':
+				case 'https:':
+				case 'www':
+					return false;
+				default:
+					return true;
+			}
+		}).value();
 
+		const fileName = directories.splice(-1, 1);
+		directories = directories.splice(0, directories.length - 1);
+
+		const directoryPath = dataDirectory + directories.join('/');
+		mkdirp(directoryPath);
+
+		download(req.url, directoryPath + '/' + fileName, ()=> {
+			console.log('done', req.url)
+		});
+
+		//console.log(`[${request.method}] ${request.url} ${directories}`);
+	}
 	proxy_request.addListener('response', function (proxy_response) {
 		proxy_response.addListener('data', function (chunk) {
-			response.write(chunk, 'binary');
+			res.write(chunk, 'binary');
 		});
 		proxy_response.addListener('end', function () {
-			response.end();
+			res.end();
 		});
-		response.writeHead(proxy_response.statusCode, proxy_response.headers);
+		res.writeHead(proxy_response.statusCode, proxy_response.headers);
 	});
-	request.addListener('data', function (chunk) {
+	req.addListener('data', function (chunk) {
 		proxy_request.write(chunk, 'binary');
 	});
-	request.addListener('end', function () {
+	req.addListener('end', function () {
 		proxy_request.end();
 	});
 }).listen(8080);
